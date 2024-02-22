c3a125d8bb0f07fcff327fe23e6262f942ca683e
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-02-22 18:48:19 +0100

    Remove test file that's not part of the program

--- test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-print("CUDA support: ", tf.test.is_built_with_cuda())
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
