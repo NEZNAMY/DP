6dc8f478b2daca33f9ed2416a1eff85af1c176a9
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-04-17 18:01:16 +0200

    Remove unused data sets and add a new one

--- menu/DatasetMenu.py
+++ menu/DatasetMenu.py
@@ -1,7 +1,6 @@
 import time
 
 from cnn.CNNDataSet import *
-from lstm.LSTMDataSet import LSTMDataSet
 from mlp import MLPConstructionMenu
 from mlp.MLPDataSet import MLPDataSet
 
@@ -29,34 +28,15 @@ class DatasetMenu:
         print("Loaded dataset 3 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
 
         start_time = time.time_ns()
-        dataset102 = MLPDataSet(tk, os.path.join(base_dir, 'Dataset_102_Alzheimer_Features'),
-                                "Dataset 102 - Alzheimer Features")
-        print("Loaded dataset 102 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
-
-        start_time = time.time_ns()
-        dataset104 = MLPDataSet(tk, os.path.join(base_dir, 'Dataset_104_Alzheimer_Plasma'),
-                                "Dataset 104 - Alzheimer Plasma lipidomics")
-        print("Loaded dataset 104 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
-
-        start_time = time.time_ns()
-        dataset105 = MLPDataSet(tk, os.path.join(base_dir, 'Dataset_105_Parkinson_Features'),
-                                "Dataset 105 - Parkinson Features")
-        print("Loaded dataset 105 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
-
-        start_time = time.time_ns()
-        dataset99 = LSTMDataSet(tk, os.path.join(base_dir, 'Dataset_99_Test'), "Dataset 99 - Test")
-        print("Loaded dataset 99 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
+        dataset4 = MLPDataSet(tk, os.path.join(base_dir, 'Dataset_4_Finger_Tapping'),
+                              "Dataset 4 - Finger Tapping")
+        print("Loaded dataset 4 in " + str(int((time.time_ns() - start_time) / 1000000)) + "ms")
 
         self.addDataSet(dataset1, "Dataset 1 - Parkinson drawing [CNN]")
         self.menu.add_separator()
         self.addMLPDataSet(dataset2, "Dataset 2 - Parkinson Speech [MLP]")
         self.addMLPDataSet(dataset3, "Dataset 3 - Alzheimer Handwriting [MLP]")
-        self.addMLPDataSet(dataset102, "Dataset 102 - Alzheimer Features [MLP]")
-        self.addMLPDataSet(dataset104, "Dataset 104 - Alzheimer Plasma [MLP]")
-        self.addMLPDataSet(dataset105, "Dataset 105 - Parkinson Features [MLP]")
-
-        self.menu.add_separator()
-        self.addDataSet(dataset99, "Dataset 99 - Test [LSTM]")
+        self.addMLPDataSet(dataset4, "Dataset 4 - Finger Tapping [MLP]")
 
     def addMLPDataSet(self, dataSet: MLPDataSet, displayName: str):
         self.addDataSet(dataSet, displayName)
